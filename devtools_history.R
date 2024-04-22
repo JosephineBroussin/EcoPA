6d5666fd976b24c8e01b0b23d8c5676bfb61d40b
JosephineBroussin <josephine.broussin@mnhn.fr>
2024-04-22 11:15:21 +0200

    full package commit

---
+++ devtools_history.R
@@ -0,0 +1,17 @@
+#### A history of what i runned in the console to build the package
+
+
+## What document to ignore in the package (this one beacause it is just to keep track of what ive done)
+usethis::use_build_ignore("devtools_history.R")
+
+
+## What package we load as necessary in our package (used in our functions)
+usethis::use_package("dplyr")
+usethis::use_package("plyr")
+usethis::use_package("magrittr")
+usethis::use_package("stats")
+usethis::use_package("graphics")
+
+
+## Generate documentations for each function (based on the roxygen skeleton created on top of each function)
+devtools::document()
