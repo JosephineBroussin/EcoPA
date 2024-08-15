88c04d436dedb81b7b16dcaf7d74b9614f90db04
JosephineBroussin <122543506+JosephineBroussin@users.noreply.github.com>
2024-08-15 15:37:57 +0200

    Bathy_Example_VS

--- Exemple_Virtual_Species/1-Creation_of_the_Virtual_Species.R
+++ Exemple_Virtual_Species/1-Creation_of_the_Virtual_Species.R
@@ -23,20 +23,28 @@ sal = stack("Salinity_climato_2000-2020.grd")
 wind = stack("Wind_climato_2000-2020.grd")
 ice = stack("Ice_climato_2000-2020.grd")
 
+# The bathymetry is added only to remove the high sea areas in the suitability estimation
+# and therefore in the presences. If these zones (the majority in terms of size, with SBT values
+# between 0 and 3°C) are included, the majority of presences will have low SBT values, leading to a skewed response curve.
+bathy = stack("Bathy.grd")
 
 ### Creation of the responses curves (gaussian)
 my_parameters <- formatFunctions(SBT = c(fun = 'dnorm', mean = 0, sd = 15),
                                  SST = c(fun = 'dnorm', mean = 0, sd = 15),
                                  Salinity = c(fun = 'dnorm', mean = 35, sd = 10),
                                  Ice = c(fun = 'dnorm', mean = 0, sd = 0.5),
-                                 Wind = c(fun = 'dnorm', mean = 0, sd = 10))
+                                 Wind = c(fun = 'dnorm', mean = 0, sd = 10),
+                                 Bathy = c(fun = 'betaFun', p1 = -200, p2 = 0, alpha = 0.9, gamma = 0.08))
 
 
 ########## 2) Environment suitability according to those responses curves ##########
-my_env = stack(sbt, sst, sal, wind, ice)
+my_env = stack(sbt, sst, sal, wind, ice, bathy)
+
+## Focus on the North-East Atlantic
+my_env = crop(my_env,  extent(-35,50,36,90))
 
 
-virtspecies = generateSpFromFun(raster.stack = my_env[[c("SBT","SST", "Salinity", "Wind", "Ice")]],
+virtspecies = generateSpFromFun(raster.stack = my_env[[c("SBT","SST", "Salinity", "Wind", "Ice", "Bathy")]],
                                 parameters = my_parameters,
                                 plot = TRUE)
 
@@ -66,6 +74,7 @@ for(i in 1:nlayers(my_env)){
   coord_presences[,i+2] = raster::extract(my_env[[i]], coord_presences[,1:2])
 }
 
+coord_presences = coord_presences[,-c(ncol(coord_presences))]
 
 colnames(coord_presences) =
   c("X", "Y", "SBT", "SST", "Salinity", "Wind", "Ice")
